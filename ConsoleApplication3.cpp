ab0bf8d09cbe8fb4be07f8d2f31aa2e8f17ca278
FairuzDiana <55312572+FairuzDiana@users.noreply.github.com>
2019-10-22 09:57:42 +0800

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,29 @@
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+void swap(int a, int b);
+
+int main()
+{
+	int a, b;
+	cout << "please enter 2 integer: " << endl;
+	cin >> a >> b;
+	swap(a,b);
+	return 0;
+}
+
+void swap(int a, int b)
+{
+	int c, d;
+	cout << "you entered a is " << a << endl;
+	cout << "you entered b is " << b << endl;
+
+	c = b;
+	d = a;
+
+	cout << "after swap the value a now is " << c << endl;
+	cout << "after swap the value b now is " << a << endl;
+
+}
